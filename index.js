5df5fad5411957b336e497d5ed1db7b9b11b70a8
Mohammedalduraidi <mohd.alduraidi@gmail.com>
2018-07-05 02:47:33 +0300

    fix code styling in index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 
 // View Engine
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile)
+app.engine('html', require('ejs').renderFile);
 
 app.use(express.static(path.join(__dirname , 'views')));
 
 app.get('/', (req, res)=>{
-  res.send('index')
-})
+	res.send('index');
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('app listening on port ya alo! ', port))
+app.listen(port, () => console.log('app listening on port ya alo! ', port));
